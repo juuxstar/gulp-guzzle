cf4a158ce3d357ec54bfea0f5d8627b1eae352e9
Tomas <tomas@roadmunk.com>
2019-07-22 17:13:38 -0400

    adding taskOnce and better printout option

--- guzzle.js
+++ guzzle.js
@@ -7,9 +7,17 @@ const path               = require('path');
 const Viz                = require('viz.js');
 const { Module, render } = require('viz.js/full.render.js');
 const _                  = require('lodash');
+const clc                = require('cli-color');
 
 const guzzleTasks = [];
 let graphFilename;
+
+const State = {
+	NOT_STARTED: 'not_started',
+	STARTED    : 'started',
+	DONE       : 'done',
+	ERROR      : 'error'
+};
 
 const oldStart = gulp.start;
 gulp.start     = function() {
@@ -27,22 +35,41 @@ gulp.start     = function() {
  * @param {Object} [options]
  * @param {String} [options.taskGraph]  - if present, the name of the file to output a task graph in the format of the extension
  *                             (see https://github.com/mdaines/viz.js/wiki/API for available extensions/formats)
- * @param {Function} [options.onFinish] - is called everytime there are no more tasks left to run (can occur multiple times due to watch)
+ * @param {Function} [options.onFinish] - is called every time there are no more tasks left to run (can occur multiple times due to watch)
+ * @param {Boolean}  [options.prettyPrint] if true, uses a "different" display of tasks (experimental)
  */
-const guzzle = module.exports = function(plugins, options) {
-	options = options || {};
-
-	if (options.taskGraph) {
-		graphFilename = options.taskGraph;
+const guzzle = module.exports = function(plugins, { taskGraph, onFinish, prettyPrint = false } = {}) {
+	if (taskGraph) {
+		graphFilename = taskGraph;
 	}
 
-	if (options.onFinish) {
+	if (onFinish) {
 		gulp.on('task_stop', _.debounce(() => {
 			if (!_.some(gulp.tasks, t => t.running)) {
-				options.onFinish();
+				onFinish();
 			}
 		}, 100));
 	}
+
+	gulp.on('task_start', event => {
+		const task    = findGuzzleTask(event.task);
+		task._state   = State.STARTED;
+		task._started = new Date();
+		if (prettyPrint) { printTasks(); }
+	});
+
+	gulp.on('task_stop', event => {
+		const task = findGuzzleTask(event.task);
+		if (task._state === State.DONE) return;
+		task._state = State.DONE;
+		task._ended = new Date();
+		if (prettyPrint) { printTasks(); }
+	});
+
+	gulp.on('task_err', event => {
+		findGuzzleTask(event.task)._state = State.ERROR;
+		if (prettyPrint) { printTasks(); }
+	});
 
 	for (const name in plugins) {
 		GuzzleTask.prototype[name] = makeGuzzlePlugin(plugins[name]);
@@ -56,29 +83,35 @@ const guzzle = module.exports = function(plugins, options) {
 	}
 };
 
-guzzle.task = function(name, depends, fn) {
-	// check if called without any dependencies
-	if (arguments.length === 2 && typeof depends == 'function') {
-		fn      = depends;
-		depends = [];
-	}
 
-	if (!Array.isArray(depends)) {
-		depends = [ depends ];
-	}
-
-	const guzzleTask = new GuzzleTask(name, _.compact(depends), fn);
-	guzzleTasks.push(guzzleTask);
-	return guzzleTask;
+/**
+ * @param {Object} [options]
+ * @param {Boolean} [options.runOnce] if true, task is only ever executed once
+ */
+guzzle.task = function(name, depends, doneCallback, options={}) {
+	const { name, depends, doneCallback, options } = parseTaskArguments(...arguments)
+	return new GuzzleTask(name, depends, doneCallback, options);
 };
+
+/**
+ * A variant of guzzle.task that only runs once.
+ */
+guzzle.taskOnce = function(name, depends, doneCallback, options={}) {
+	const { name, depends, doneCallback, options } = parseTaskArguments(...arguments);
+	options.runOnce = true;
+	return guzzle.task(name, depends, doneCallback, options);
+}
 
 guzzle.watch = gulp.watch.bind(gulp);
 
 // Guzzle Task
 class GuzzleTask {
-	constructor(name, dependencies, onDoneFunction) {
+	constructor(name, dependencies, onDoneFunction, { runOnce = false }={}) {
 		this._name         = name;
 		this._stream       = null;
+		this._started      = null;
+		this._state        = State.NOT_STARTED;
+		this._runOnce      = runOnce;
 		this._dependencies = dependencies.map(dependantTask => {
 			// for any task dependencies not specified by name, assume they're sub-tasks and prepend parent task name
 			if (dependantTask instanceof GuzzleTask) {
@@ -88,8 +121,14 @@ class GuzzleTask {
 			return dependantTask;
 		});
 
+		guzzleTasks.push(this);
+
 		if (onDoneFunction) {
 			this._onDoneFunction = callback => {
+				if (this._runOnce && this._state !== State.NOT_STARTED) {
+					return callback();
+				}
+
 				const result = onDoneFunction.call(this, callback);
 
 				// reset stream in case the task is run again (via watch)
@@ -186,3 +225,43 @@ async function outputTaskGraph() {
 	}));
 }
 
+function findGuzzleTask(taskName) {
+	return guzzleTasks.find(task => task._name === taskName);
+}
+
+function printTasks() {
+	const now = new Date();
+	const stateSymbols = {
+		[State.NOT_STARTED]: '-',
+		[State.STARTED]    : clc.yellow('?'),
+		[State.DONE]       : clc.green.bold('✓'),
+		[State.ERROR]      : clc.redBright('X')
+	}
+
+	// clear the screen and print out updated list of tasks
+	process.stdout.write(
+		'\033c\n'		// clears terminal
+		+ guzzleTasks.map(task => {
+			let elapsed = task._started ? (task._ended || now) - task._started : 0;
+			elapsed     = elapsed > 1000 ? (Math.round(elapsed / 100) / 10) + 's' : elapsed + 'ms';
+			return `${stateSymbols[task._state]} ${task._name} ${elapsed ? clc.blackBright(`(${elapsed})`) : ''}`;
+		}).join('\n')
+		+ '\n---\n'
+	);
+}
+
+function parseTaskArguments(name, depends, doneCallback, options={}) {
+	// check if called without any dependencies
+	if (arguments.length === 2 && typeof depends === 'function') {
+		doneCallback = depends;
+		depends      = [];
+	}
+
+	if (!Array.isArray(depends)) {
+		depends = [ depends ];
+	}
+
+	depends = _.compact(depends);
+
+	return { name, depends, doneCallback, options};
+}
